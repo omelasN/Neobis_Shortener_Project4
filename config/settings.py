9aa6d4e9a18e89c6aaa5b35feb2e99a7499046e4
omelasN <semeteeva.n@gmail.com>
2023-11-15 21:26:03 +0600

    add postgresql to project

--- config/settings.py
+++ config/settings.py
@@ -76,11 +76,16 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'url',
+        'USER': 'postgres',
+        'PASSWORD': '0405',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
